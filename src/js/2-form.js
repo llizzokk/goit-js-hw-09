6848966cb5635668ca4c2ce570e407d073146200
Liza <lizaapostol378@gmail.com>
2024-10-18 14:54:43 +0300

    finished tasks

---
+++ src/js/2-form.js
@@ -0,0 +1,45 @@
+'use strict';
+
+let formData = {
+  email: '',
+  message: '',
+};
+
+const form = document.querySelector('.feedback-form');
+
+function handlerInput() {
+  const savedData = localStorage.getItem('feedback-form-state');
+  if (savedData) {
+    formData = JSON.parse(savedData);
+    form.elements.email.value = formData.email;
+    form.elements.message.value = formData.message;
+  }
+}
+
+form.addEventListener('input', event => {
+  const { name, value } = event.target;
+  formData[name] = value;
+  localStorage.setItem('feedback-form-state', JSON.stringify(formData));
+});
+
+form.addEventListener('submit', handleSubmit);
+
+function handleSubmit(event) {
+  event.preventDefault();
+
+  const elements = event.currentTarget.elements;
+
+  if (
+    elements.email.value.length === 0 ||
+    elements.message.value.length === 0
+  ) {
+    alert('Fill please all fields');
+  }
+  console.log(formData);
+
+  localStorage.removeItem('feedback-form-state');
+  formData = { email: '', message: '' };
+  form.reset();
+}
+
+handlerInput();
